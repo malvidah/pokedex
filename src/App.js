fe3ba35375edb4ad62ea279cc89c33001c8bbb50
mindnmattr <marvin.liyanage@gmail.com>
2021-03-25 22:50:47 -0700

    Hello World
    added the logo

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from '/Users/marvinliyanage/Desktop/2 Projects/Web Development/pokedex/src/curytering.png';
 import './App.css';
 
 function App() {
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Curyte
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="marvinliyanage.com"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Login
         </a>
       </header>
     </div>
